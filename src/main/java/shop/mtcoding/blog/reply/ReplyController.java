848c7ac0c5d6833ef12cd074bbd1f9815e5381ba
Pakchan <1625787@donga.ac.kr>
2024-02-14 17:40:09 +0900

    ãDTO 만들기

--- src/main/java/shop/mtcoding/blog/reply/ReplyController.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyController.java
@@ -2,12 +2,14 @@ package shop.mtcoding.blog.reply;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // 댓글쓰기, 댓글삭제, 댓글 목록보기
 @RequiredArgsConstructor
 @Controller
 public class ReplyController {
 
+    @PostMapping("/reply/save")
     public void write(){
 
     }
